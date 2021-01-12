4d363382b8ce3ea93633d3dc14b4f54b08ce8ce2
小野和論 <matopenkw@gmail.com>
2021-01-13 00:43:34 +0900

    add find

---
+++ app/impl/get_posts.go
@@ -0,0 +1,29 @@
+package impl
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+type Post struct {
+	ID      int
+	Content string
+	author  string
+}
+
+func GetPosts(db *gorm.DB, c *gin.Context) {
+	var ps []*Post
+	err := db.Find(&ps).Error
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": "dbError",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"posts": ps,
+	})
+}
