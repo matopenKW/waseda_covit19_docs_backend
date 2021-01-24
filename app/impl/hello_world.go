850b662fd83d92ce2243798dbd98e2d65a97340e
小野和論 <matopenkw@gmail.com>
2021-01-25 01:30:46 +0900

    add responceimpl

--- app/impl/hello_world.go
+++ app/impl/hello_world.go
@@ -1,27 +1,17 @@
 package impl
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/matopenKW/waseda_covit19_docs_backend/app/repository"
 )
 
-type HelloWorldImpl interface {
-	HelloWorld(ctx *gin.Context) (*HelloWorldResponce, error)
-}
-
 type HelloWorldRequest struct {
 	Name string
 }
 
-type HelloWorldResponce struct {
-	Message string
-}
-
-func HelloWorld(con repository.Connection, c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello world",
-	})
+func HelloWorld(con repository.Connection, c *gin.Context) (ResponceImpl, error) {
+	return &HelloWorldResponce{
+		Message: "hello world",
+	}, nil
 }
--- app/impl/update_post.go
+++ app/impl/update_post.go
@@ -1,15 +1,13 @@
 package impl
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/matopenKW/waseda_covit19_docs_backend/app/model"
 	"github.com/matopenKW/waseda_covit19_docs_backend/app/repository"
 )
 
-func UpdatePost(con repository.Connection, ctx *gin.Context) {
+func UpdatePost(con repository.Connection, ctx *gin.Context) (ResponceImpl, error) {
 
 	result, err := con.SavePost(&model.Post{
 		ID:      1,
@@ -17,13 +15,9 @@ func UpdatePost(con repository.Connection, ctx *gin.Context) {
 		Author:  "author1-1",
 	})
 	if err != nil {
-		ctx.JSON(http.StatusOK, gin.H{
-			"error": "dbError",
-		})
-		return
+		return nil, err
 	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"post": result,
-	})
+	return &UpdatePostResponce{
+		Post: result,
+	}, nil
 }
