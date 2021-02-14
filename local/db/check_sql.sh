d7451d5de3cdb6e164c6fece329befd323c4f5f3
小野和論 <matopenkw@gmail.com>
2021-02-14 13:42:36 +0900

    add go test

--- local/db/check_sql.sh
+++
@@ -1,9 +0,0 @@
-#bin/bash
-
-for f in ./sql/*; do
-    SQL_FILE=$(basename ${f%.*})
-    docker exec -it local_db /bin/bash -c "PGPASSWORD=gwp psql gwp -U gwp -f var/local/${SQL_FILE}.sql"
-
-    docker exec -it local_db /bin/bash -c 'false; echo $? > /tmp/return-code'
-    echo $(docker exec local_db /bin/cat /tmp/return-code)
-done
